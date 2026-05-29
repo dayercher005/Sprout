b01c67cab619d2cef59187a7d1a3ae53af93ab0d
Dayer <dayerxr.cher@gmail.com>
2026-05-29 18:13:11 +0800

    fix: debugging next-auth errors

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,22 +1,6 @@
 import NextAuth from "next-auth";
-import Google from "next-auth/providers/google";
-import Github from "next-auth/providers/github";
-import {
-  GOOGLE_CLIENT_ID,
-  GOOGLE_CLIENT_SECRET,
-  GITHUB_CLIENT_ID,
-  GITHUB_CLIENT_SECRET
-} from "@/config";
+import { authOptions } from "@/libs/auth";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [
-    Google({
-      clientId: GOOGLE_CLIENT_ID,
-      clientSecret: GOOGLE_CLIENT_SECRET
-    }),
-    Github({
-      clientId: GITHUB_CLIENT_ID,
-      clientSecret: GITHUB_CLIENT_SECRET
-    })
-  ]
-});
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
